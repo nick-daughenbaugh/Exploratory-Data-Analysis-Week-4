479d3ea01d84e9628a405e050ed0da10823cc3c8
Nicholas Daughenbaugh <nicholas.r.daughenbaugh.civ@mail.mil>
2020-02-25 13:35:40 -0700

    20200225.1335

---
+++ plot1.R
@@ -0,0 +1,22 @@
+# Load Packages
+library("data.table")
+
+# Disable Scientific Notation
+options(scipen=999)
+
+# download data
+if ( ! file.exists("summarySCC_PM25.rds")  ) {
+  download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", "raw_data.zip")
+  unzip("raw_data.zip")
+  file.remove("raw_data.zip")
+}
+
+#Read-in Data / Set as Data Table
+PM25 <- readRDS("summarySCC_PM25.rds")
+setDT(PM25)
+
+#Format / Summarize Data
+total_PM25 <- PM25[,sum(Emissions), by = year][, Total_Emissions := V1][,V1 := NULL]
+
+#Plot Data
+barplot(total_PM25[, Total_Emissions], xlab = "Years", names = total_PM25[, year] ,ylab = "Total Emissions", main = "Total Emissions by Years")
